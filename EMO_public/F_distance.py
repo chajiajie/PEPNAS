ec38983cb1fe27128de4c447788c92b10e474211
chajiajie <56816450+chajiajie@users.noreply.github.com>
2023-11-17 13:46:40 +0800

    Update F_distance.py

--- EMO_public/F_distance.py
+++ EMO_public/F_distance.py
@@ -3,14 +3,12 @@ import numpy as np
 def F_distance(FunctionValue,FrontValue):
     N, M = FunctionValue.shape
     CrowdDistance = np.zeros((1, N))
-    # CrowdDistance_temp = CrowdDistance
-    #差集： list(set(a)-set(b)), list(set(a).difference(set(b)))
     temp=np.unique(FrontValue)
     Fronts = temp[temp != np.inf]
 
 
     for f in range(len(Fronts)):
-        Front = np.where(FrontValue == Fronts[f] )[1]# array 返回 两个值 ，前面的一维的坐标，后面是二维的坐标
+        Front = np.where(FrontValue == Fronts[f] )[1
         Fmax = np.max(FunctionValue[Front, :], axis=0)
         Fmin = np.min(FunctionValue[Front, :], axis=0)
         for i in range(M):
