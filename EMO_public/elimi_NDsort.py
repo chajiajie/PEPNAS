8900fa7b3f1b0fed933991309838931c1915cf36
chajiajie <56816450+chajiajie@users.noreply.github.com>
2023-11-17 13:31:26 +0800

    Create elimi_NDsort.py

---
+++ EMO_public/elimi_NDsort.py
@@ -0,0 +1,33 @@
+import numpy as np
+from EMO_public import sortrows
+def NDSort(PopObj,Remain_Num):
+
+    N,M = PopObj.shape#（20，2）
+    FrontNO = np.inf*np.ones((1, N))
+    MaxFNO = 0
+    PopObj, rank = sortrows.sortrows(PopObj,order="descend")
+
+
+    while (np.sum(FrontNO < np.inf) < Remain_Num):
+        MaxFNO += 1
+        for i in range(N):#N：20
+            if FrontNO[0, i] == np.inf:
+                Dominated = False
+                for j in range(i-1, -1, -1):
+                    if FrontNO[0, j] == MaxFNO:
+                        m=2
+                        while (m <= M) and (PopObj[i, m-1] >= PopObj[j, m-1]):
+                            m += 1
+                        Dominated = m > M
+                        if Dominated or (M == 2):
+                            break
+                if not Dominated:
+                    FrontNO[0,i] = MaxFNO
+    # temp=np.loadtxt("temp.txt")
+    # print((FrontNO==temp).all())
+    front_temp = np.zeros((1,N))
+    front_temp[0, rank] = FrontNO
+    # FrontNO[0, rank] = FrontNO
+
+
+    return front_temp, MaxFNO
